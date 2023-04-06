bfb5e54da9b20ed551f76e723427770b8f31ee2b
eddieg00 <119461197+eddieg00@users.noreply.github.com>
2023-04-05 20:26:52 -0400

    did all the work while everyone slept

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes')
+const apiRoutes = require('./apiRoutes');
 
 router.use('/users', userRoutes);
 router.use('/post', postRoutes);
-router.use('/comment', commentRoutes)
+router.use('/comment', commentRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
