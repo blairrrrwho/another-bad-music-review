dd8160f154d66ee8f863fbf7b9522effc12dd34a
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-04-10 15:52:03 -0500

    got lots of things working on the backend; need to make the comment function work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
-// const commentRoutes = require('./commentRoutes')
+const commentRoutes = require('./commentRoutes')
 const apiRoutes = require('./apiRoutes');
 
 router.use('/users', userRoutes);
 router.use('/post', postRoutes);
-// router.use('/comment', commentRoutes);
+router.use('/comment', commentRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
